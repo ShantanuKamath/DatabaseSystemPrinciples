832d3d48583f582439b464685b55ceec58bc813c
Simran Khare <simran001@e.ntu.edu.sg>
2017-11-17 02:00:34 +0800

    Add file

---
+++ Assignment 2/parser/sort.py
@@ -0,0 +1,53 @@
+import json
+import parser
+
+def sort(plan, start=False):
+    
+	sentence = ""
+    if "Plans" in plan:
+        for child in plan["Plans"]:
+            tmp = parse_plan(child, start)
+            sentence += tmp + " "
+            if start:
+                start = False
+
+    if plan["Node Type"] == "Sort":
+        sentence += parser.get_conjuction(start)
+        sentence += "the result is sorted on the attribute "
+        if "DESC" in plan["Sort Key"]:
+            sentence += str(plan["Sort Key"].replace('DESC', '')) +" in desceding order."
+        elif "INC" in plan["Sort Key"]:
+            sentence += str(plan["Sort Key"].replace('INC', '')) +" in increasing order."
+        else:
+            sentence += str(plan["Sort Key"])+ "."
+    return sentence
+
+
+if __name__ == "__main__":
+    test = '''
+   {                                             
+         "Node Type": "Sort",               
+           "Parent Relationship": "Outer",    
+           "Parallel Aware": false,           
+           "Startup Cost": 31061.74,          
+           "Total Cost": 32518.24,            
+           "Plan Rows": 582599,               
+           "Plan Width": 100,                 
+           "Sort Key": ["title"],             
+           "Plans": [                         
+             {                                
+               "Node Type": "Seq Scan",       
+               "Parent Relationship": "Outer",
+               "Parallel Aware": false,       
+               "Relation Name": "publication",
+               "Alias": "publication",        
+               "Startup Cost": 0.00,          
+               "Total Cost": 15558.99,        
+               "Plan Rows": 582599,           
+               "Plan Width": 100              
+             }                                
+           ]           
+     }
+    '''
+    test_plan = json.loads(test)
+    print(sort(test_plan, start=True))
