c322830104057f41d5e2bb7fba5741d87b9a1aab
Aashika Saju <Aashika@Aashika.local>
2017-11-17 01:49:41 +0800

    Added MergeJoin and NestedLoop

---
+++ Assignment 2/parser/mergeJoin.py
@@ -0,0 +1,20 @@
+import utils, parser
+
+
+def merge_join(plan, start=False):
+    text = ""
+    if 'Plans' in plan:
+        for successor in plan['Plans']:
+            text += parser.parse_plan(successor, start) + " "
+            if start:
+                start = False
+    text += utils.get_conjuction(start)
+    text += "merge join is employed to merge the preceeding result"
+    if 'Merge Cond' in plan:
+        text += " with the following condition: " + plan['Merge Cond'].replace("::text", "")
+    if 'Join Type' == 'Semi':
+        text += " however, the only row returned is that from the left table"
+    else:
+        text += "."
+
+    return text
---
+++ Assignment 2/parser/nestedLoopJoin.py
@@ -0,0 +1,11 @@
+import utils, parser
+
+def nested_loop_join(plan, start=False):
+	text = ""
+	start_text = parser.parse_plan(plan["Plans"][0], start)
+	text += start_text
+	start_text += parser.parse_plan(plan["Plans"][1])
+	text += " " + start_text
+	text += " the results of the nested loop join are returned in new rows."
+	return text
+
